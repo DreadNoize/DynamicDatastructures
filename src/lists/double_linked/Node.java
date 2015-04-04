ccff0049bac41bd38e8dd2571268c4a7ee89f868
Rafael Bachmann <triolenzunge@gmail.com>
2015-04-04 16:10:18 +0200

    initial commit of Node with prev and next

---
+++ src/lists/double_linked/Node.java
@@ -0,0 +1,54 @@
+package lists.double_linked;
+
+/**
+ * Created by ra on 02.04.15. Simple generic linked list.
+ */
+
+public class Node<T> {
+	public Node<T> next;
+	public Node<T> prev;
+	private T data;
+
+	public Node(T data) {
+		this.data = data;
+		this.next = null;
+		this.prev = null;
+	}
+
+	public Node(T data, Node<T> prev, Node<T> next) {
+		this.data = data;
+		this.prev = prev;
+		this.next = next;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+
+	}
+
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+	
+	public void setPrev(Node<T> prev) {
+		this.prev = prev;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public String toString() {
+		return data.toString();
+	}
+
+	public Node<T> cloneRec(DLList<T> parent) {
+		System.out.println(this);
+		if (this.next != null)
+			return new Node<T>(data, this, next.cloneRec(parent));
+		else {
+			return new Node<T>(data, this, new Node<T>(parent.getRear().getData()));
+		}
+	}
+
+}
