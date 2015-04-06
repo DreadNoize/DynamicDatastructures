e13417105e573783680de3bd9751bf8d2646dc16
Rafael Bachmann <triolenzunge@gmail.com>
2015-04-06 20:12:32 +0200

    add test class for polymorphic list

---
+++ src/lists/polymorph/TestPolymorphicList.java
@@ -0,0 +1,12 @@
+package lists.polymorph;
+
+public class TestPolymorphicList {
+
+	public static void main(String[] args) {
+		
+		PolymorphicList bunch = new PolymorphicList(43, "extra", true, 40);
+		System.out.println(bunch);
+
+	}
+
+}
