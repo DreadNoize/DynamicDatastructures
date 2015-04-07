6f83d24db7ae7b655401fa79c56ca6845f834807
Rafael Bachmann <triolenzunge@gmail.com>
2015-04-07 19:37:56 +0200

    Corrected typo in filename

---
+++ src/lists/IndexOutOfBoundsException.java
@@ -0,0 +1,15 @@
+package lists;
+
+public class IndexOutOfBoundsException extends RuntimeException {
+
+	private static final long serialVersionUID = -3061382900186543648L;
+
+	public IndexOutOfBoundsException () {
+		super("Index out of bounds!");
+	}
+
+	public IndexOutOfBoundsException (String message) {
+		super("Index out of bounds! " + message);
+	}
+
+}
