2dfa090089a5d4fcc6a02be4f92b7b2fdaee2d46
Rafael Bachmann <triolenzunge@gmail.com>
2015-05-22 17:49:54 +0200

    update test calls

--- src/lists/mini/double_linked/DLLTest.java
+++ src/lists/mini/double_linked/DLLTest.java
@@ -1,17 +1,23 @@
 package double_linked;
 
 // this requires the -ea (enable assertions) flag
-// go to 'Run → Run conf → arguments tab → vm options field
+// in eclipse go to 'Run → Run conf → arguments tab → vm options field
 
 public class DLLTest {
     public static void main(String[] args) {
         DLList<Object> test = new DLList<Object>("hallo", "hier", "ist was noch fehlt:", "remove() methode", "diese Aussage ist:", true); // varargs
         assert !test.isEmpty();
         System.out.println(test.getSize());
+        System.out.println(test.get(0));
+        System.out.println(test.get(1));
+        System.out.println(test.get(2));
+        System.out.println(test.get(3));
         System.out.println(test.get(4));
+        System.out.println(test.get(5));
         test.remove(5);
         test.add(false); // werden boolean werte eigentlich geautoboxt!?
         System.out.println(test);
+        System.out.println(test.printTypes());
 
     }
 }
