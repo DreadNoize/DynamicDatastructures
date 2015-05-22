5add1476b3578caf6351095394582d6571818680
Rafael Bachmann <triolenzunge@gmail.com>
2015-05-22 15:33:06 +0200

    remove unnecessary object removal null busting in DLList.remove method

--- src/lists/mini/double_linked/DLList.java
+++ src/lists/mini/double_linked/DLList.java
@@ -63,15 +63,6 @@ public class DLList<T> {
 		return getNode(index).getData();
 	}
 
-	/*
-	 * Der gc löscht ja objekte auf die keine referenz mehr besteht. prüft er
-	 * dabei ob die referenzen aus dem nirvana kommen? Wenn aus irgendeinem
-	 * grund eine node nicht collected wird und der nachfolger dieser node
-	 * gelöscht wird, zeigt die erste noch immer mit next auf die zuletzt
-	 * gelöschte. Warum sollte der gc die node nicht löschen? Keine ahnung, aber
-	 * man sagt ihm seltsames verhalten nach. Die gc-zeilen können gerne weg, es
-	 * macht nicht viel sinn...
-	 */
 	public void remove(int index) {
     	Node<T> toRemove = getNode(index); // if index is invalid: get() will throw error.
     	if (toRemove == front && toRemove == rear) {
@@ -80,17 +71,14 @@ public class DLList<T> {
         else if (toRemove == front) {
             toRemove.next.prev = null;  // detach front
             front = toRemove.next;      // set front
-            toRemove.next = null;       // necessary for gc?
         }
         else if (toRemove == rear) {
             toRemove.prev.next = null;  // detach rear
             rear = toRemove.prev;       // set rear
-            toRemove.prev = null;       // necessary for gc?
         }
         else {
             toRemove.prev.next = toRemove.next;
             toRemove.next.prev = toRemove.prev;
-            toRemove.prev = toRemove.next = null; // gc?
         }
     size--;
     }
