dc35a98f3ae051190feab0b230dcdfae0f50d87c
Rafael Bachmann <triolenzunge@gmail.com>
2015-05-23 11:31:50 +0200

    code drop again - somewhat working enlarge() implementation

--- src/lists/mini/ringpuffer/Ringpuffer.java
+++ src/lists/mini/ringpuffer/Ringpuffer.java
@@ -1,59 +1,79 @@
 package ringpuffer;
 
+import java.util.Arrays;
+
 public class Ringpuffer {
-	private int   currentSize;
+	private int arrSize;
 	private int[] arli;
-	private int   first = 0;
-	private int   end   = 0;
+	private int first = 0;
+	private int end = 0;
+	private int size = 0;
 
 	public Ringpuffer(int cap) {
-		if(cap > 0) {
-		currentSize = cap;
-		arli = new int[currentSize];
-		arli[0] = 0; // 0 is poison value
+		if (cap > 0) {
+			arrSize = cap;
+			arli = new int[arrSize];
+			arli[0] = -1; // -1 definition of empty
 		}
 	}
 
-	private boolean isFull() { // reclaim somehow? isHalfFull() is tricky
-		return first == end && arli[first] != 0;
+	private boolean isEmpty() {
+		if (size == 0) {
+			return first == end && arli[first] == -1;
+		}
+		return false;
 	}
 
-	private boolean isEmpty() {
-		return first == end && arli[first] == 0;
+	private boolean isFull() { // reclaim somehow? isHalfFull() is tricky
+		if (size == arrSize) {
+			System.out.println(first == end % arrSize && arli[first] != -1);
+			return first == end && arli[first] != -1; // maybe more checking
+														// later?
+		}
+		return false; // a little dangerous when there are inconsistencies
+						// between size and arrSize
 	}
 
 	public void offer(int data) {
 		if (!isFull()) {
-		arli[end++ % currentSize] = data;
+			arli[end] = data;
+			end = (end + 1) % arrSize;
 		} else {
 			enlarge();
 			offer(data);
 		}
+		size++;
 	}
 
 	private void enlarge() {
-		currentSize *= 2;
-		System.out.println("enlarging to " + currentSize);
-		int[] newArr = new int[currentSize];
+		arrSize *= 2;
+		System.out.print("enlarging to ");
+		int[] newArr = new int[arrSize];
 		System.arraycopy(arli, 0, newArr, 0, arli.length);
 		arli = newArr;
+		System.out.println(Arrays.toString(arli));
 		newArr = null;
 	}
 
 	public int poll() {
 		if (!isEmpty()) {
-		int temp = arli[first];
-		arli[first] = 0;
-		first = (first + 1) % currentSize;
-		return temp;
+			int temp = arli[first];
+			arli[first] = -1;
+			first = (first + 1) % arrSize;
+			size--;
+			return temp;
 		}
 		return -1;
 	}
 
 	private int peek() {
-		if(!isEmpty()) {
+		if (!isEmpty()) {
 			return arli[first];
 		}
 		return -1;
 	}
 }
+
+// TODO: emptyval = -1, poisonVal -1?
+// TODO: reclaim space by size
+// TODO: when enlarging put stuff in the beginning of array
--- src/lists/mini/ringpuffer/testBuffer.java
+++ src/lists/mini/ringpuffer/testBuffer.java
@@ -3,10 +3,10 @@ package ringpuffer;
 public class testBuffer {
 
 	public static void main(String[] args) {
-		Ringpuffer r1 = new Ringpuffer(20);
+		Ringpuffer r1 = new Ringpuffer(5);
 
 		for (int i = 0; i < 16; i++) {
-			r1.offer((int)(Math.random()*10));
+			r1.offer(i);
 		}
 		for (int i = 0; i < 11; i++) {
 			System.out.println(r1.poll());
